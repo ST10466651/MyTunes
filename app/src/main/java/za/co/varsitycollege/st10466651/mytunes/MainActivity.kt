a5eb1236bc4a8c74621a0cc34ba10fd04c430216
ST10466651 <ST10466651@vcconnect.edu.za>
2025-06-19 10:13:31 +0200

    Updates

--- app/src/main/java/za/co/varsitycollege/st10466651/mytunes/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10466651/mytunes/MainActivity.kt
@@ -1,12 +1,32 @@
 package za.co.varsitycollege.st10466651.mytunes
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+//Mutable Array Globally Accessible
+data class Songs(
+    var title: String?,
+    var singer: String?,
+    var rate: Int?,
+    var opinion: String?
+)
+
+val songs = mutableListOf(
+    Songs("j", "sf", 3, "kjhius;ouihsrtb"),
+    Songs("j", "sf", 3, "kjhius;ouihsrtb"),
+    Songs("j", "sf", 3, "kjhius;ouihsrtb"),
+    Songs("j", "sf", 3, "kjhius;ouihsrtb")
+)
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +35,48 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Buttons Declared
+        val next = findViewById<Button>(R.id.btnNext)
+        val close = findViewById<Button>(R.id.btnClose)
+        val add = findViewById<Button>(R.id.btnAdd)
+
+        //Editable Text Declared
+        val song = findViewById<EditText>(R.id.edtSongTitle)
+        val artist = findViewById<EditText>(R.id.edtArtistName)
+        val ratings = findViewById<EditText>(R.id.edtRatings)
+        val comments = findViewById<EditText>(R.id.edtComments)
+
+        //Terminate Button
+        close.setOnClickListener {
+            finishAffinity()
+        }
+
+        //Next Page Button
+        next.setOnClickListener {
+            val intent = Intent(this, ViewScreen::class.java)
+            startActivity(intent)
+        }
+
+        //Add to Playlist Button
+        add.setOnClickListener {
+            val titles = song.text.toString().trim().lowercase()
+            val singers = artist.text.toString().trim().lowercase()
+            val rates = ratings.text.toString().trim().lowercase()
+            val opinions = comments.text.toString().trim().lowercase()
+
+
+            if (titles.isEmpty() or singers.isEmpty() or rates.isEmpty() or opinions.isEmpty()) {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Input Required")
+                builder.setMessage("Please enter required details")
+                builder.setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                builder.create().show()
+                return@setOnClickListener
+            }
         }
     }
 }
--- app/src/main/java/za/co/varsitycollege/st10466651/mytunes/View_Screen.kt
+++ app/src/main/java/za/co/varsitycollege/st10466651/mytunes/View_Screen.kt
@@ -1,12 +1,17 @@
 package za.co.varsitycollege.st10466651.mytunes
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class View_Screen : AppCompatActivity() {
+class ViewScreen : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,28 @@ class View_Screen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Buttons Declared
+        val display = findViewById<Button>(R.id.btnDisplay)
+        val back = findViewById<Button>(R.id.btnReturn)
+        val rate = findViewById<Button>(R.id.btnAvgRatings)
+
+        //TextView Declared
+        val outcome = findViewById<TextView>(R.id.txtDisplay)
+
+        //Return Button
+        back.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //Display Button
+        display.setOnClickListener {
+            outcome.text = "${songs[0].title}\n" +
+                "${songs[0].singer}\n" +
+                "${songs[0].rate}\n" +
+                "${songs[0].opinion}\n\n"
         }
     }
 }
